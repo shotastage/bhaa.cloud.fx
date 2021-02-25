dfe21be6e7c525cd9f9c55fffbd7dcd50f5f553a
Shota Shimazu <shotastage@users.noreply.github.com>
2021-02-25 12:24:08 +0900

    add proj file

--- index.js
+++
@@ -1,5 +0,0 @@
-exports.main = (req, res) => {
-    let message = req.query.message || req.body.message || 'Hello World!';
-    res.status(200).send(message);
-};
-  
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-const main = (req, res) => {
-    let message = req.query.message || req.body.message || 'Hello World!';
+import { Request, Response } from 'express';
+
+export const main = (req: Request, res: Response) => {
+    const message = req.query.message || req.body.message || 'Hello World!';
     res.status(200).send(message);
 };
-
-export default main;
